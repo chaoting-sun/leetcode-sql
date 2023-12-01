9bbda301bf3046db3906d45f39971a794a613f1e
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-12-01 11:25:55 -0600

    Time: 922 ms (37.52%), Space: 0B (100%) - LeetHub

---
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select
+    activity_date day,
+    count(distinct user_id) active_users
+from
+    Activity
+where
+    activity_date between date_sub('2019-07-27', interval 29 day) and '2019-07-27'
+group by
+    activity_date;
+
+
+/*learned
+
+(1) DATE_SUB: https://www.fooish.com/sql/mysql-date_sub-function.html
+*/
