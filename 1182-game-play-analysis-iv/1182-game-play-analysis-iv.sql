9015fa3d2499edb4617184065e63c07b696be07b
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-12-08 03:47:28 -0600

    Time: 907 ms (70.52%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,13 +1,35 @@
 # Write your MySQL query statement below
 
+
+### method1 (slow)
+
+# select
+#     round(
+#         count(*)/(select count(distinct player_id) from Activity),
+#     2) fraction
+# from
+#     Activity a1
+# where
+#     datediff(
+#         a1.event_date,
+#         (select min(event_date) from Activity a2 where a1.player_id=a2.player_id)
+#     ) = 1;
+
+
+### method2
+
 select
     round(
-        count(*)/(select count(distinct player_id) from Activity),
+        count(distinct player_id)/(select count(distinct player_id) from Activity),
     2) fraction
 from
-    Activity a1
+    Activity
 where
-    datediff(
-        a1.event_date,
-        (select min(event_date) from Activity a2 where a1.player_id=a2.player_id)
-    ) = 1;
+    (player_id, date_sub(event_date, interval 1 day)) in (
+        select
+            player_id, min(event_date)
+        from
+            Activity
+        group by
+            player_id
+    );
