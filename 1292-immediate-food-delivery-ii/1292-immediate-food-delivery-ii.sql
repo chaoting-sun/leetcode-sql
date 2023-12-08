5cf091d6dc1377fe3a84ed9fc8755e4e4e815a43
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-12-08 02:53:58 -0600

    Time: 2389 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,23 +1,43 @@
 # Write your MySQL query statement below
 
-with first_order
-as (
-    select
-        customer_id, order_date, customer_pref_delivery_date
-    from
-        Delivery d1
-    where
-        order_date = (
-            select
-                min(order_date)
-            from
-                Delivery d2
-            where
-                d1.customer_id = d2.customer_id
-        )
-)
+### method1 (slow)
+
+# with first_order
+# as (
+#     select
+#         customer_id, order_date, customer_pref_delivery_date
+#     from
+#         Delivery d1
+#     where
+#         order_date = (
+#             select
+#                 min(order_date)
+#             from
+#                 Delivery d2
+#             where
+#                 d1.customer_id = d2.customer_id
+#         )
+# )
+# select
+#     round(sum(if(order_date=customer_pref_delivery_date,1,0))/count(*)*100, 2)  
+#         as immediate_percentage
+# from
+#     first_order;
+
+
+### method2
+
 select
     round(sum(if(order_date=customer_pref_delivery_date,1,0))/count(*)*100, 2)  
         as immediate_percentage
 from
-    first_order;
+    Delivery
+where
+    (customer_id, order_date) in (
+        select
+            customer_id, min(order_date) first_order_date
+        from
+            Delivery
+        group by
+            customer_id
+    );
